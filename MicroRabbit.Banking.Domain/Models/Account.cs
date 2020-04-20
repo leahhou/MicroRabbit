26c9093293cae8a4c2bec32beaf6d7e710bcf824
leahhou <hsx_124@hotmail.com>
2020-04-20 13:47:42 +1000

    remove trial Api project and account model and accountrepository interface

--- MicroRabbit.Banking.Domain/Models/Account.cs
+++ MicroRabbit.Banking.Domain/Models/Account.cs
@@ -2,6 +2,8 @@ namespace MicroRabbit.Banking.Domain.Models
 {
     public class Account
     {
-        public int Id { get; set;  }
+        public int Id { get; set; }
+        public string AccountType { get; set; }
+        public decimal AccountBalance { get; set; }
     }
 }
