be374a30a3461c85327f543fd6bf7c7c8d6bc202
leahhou <hsx_124@hotmail.com>
2020-04-20 15:18:36 +1000

    construct BankingController

--- MicroRabbit.Banking.Api/Controllers/BankingController.cs
+++ MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -1,7 +1,26 @@
+using System.Collections.Generic;
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
 namespace MicroRabbit.Banking.Api.Controllers
 {
-    public class BankingController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BankingController : ControllerBase
     {
+        private readonly IAccountService _accountService;
+
+        public BankingController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
         
+        // GET api/banking
+        [HttpGet]
+        public ActionResult<IEnumerable<Account>> Get()
+        {
+            return Ok(_accountService.GetAccounts());
+        }
     }
 }
